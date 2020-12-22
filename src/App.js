babed8fde0caf1da93d8ab39ca81da2a0c8effd3
Chingis Ochirov <chingis09@gmail.com>
2020-12-22 18:30:14 +0800

    smooth page transitions test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { CSSTransition } from "react-transition-group";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
+import "./App.css";
+
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/about", name: "About", Component: About },
+  { path: "/contact", name: "Contact", Component: Contact },
+];
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="mx-auto">
+          {routes.map((route) => (
+            <NavLink to={route.path} activeClassName="active" exact>
+              {route.name}
+            </NavLink>
+          ))}
+        </nav>
+
+        <div className="container">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </div>
+      </div>
+    </Router>
   );
 }
 
